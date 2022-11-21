1738767517a0b6288b8b97627b4bdd280e0fc942
HappyJerry2445 <114814548+HappyJerry2445@users.noreply.github.com>
2022-11-21 17:54:53 +0100

    Update illness_user_page.py

--- view/illness_user_page.py
+++ view/illness_user_page.py
@@ -10,13 +10,13 @@ class Illness_User_Page(tk.Frame):
         tk.Frame.__init__(self, parent,bg ="#ABCDEF")
         self.controller=controller
         self.win.geometry("600x500")
-
+        
         #back button
         self.back_button=tk.Button(self,text="""  
   __    
  / /___ 
 < <(___)
- \_\  """,command=lambda : controller.show_frame("User_Page"),fg="#000000",bg="#D2D2D2")
+ \_\  """,command=lambda : self._clean(),fg="#000000",bg="#D2D2D2")
         self.back_button.place(x=0,y=10)
         
         #page title
@@ -28,13 +28,18 @@ class Illness_User_Page(tk.Frame):
         self.illness_label.place(x=125,y=150)
         self.illness_list=Listbox(self,yscrollcommand=Scrollbar.set,height=10,width=50)
         self.illness_list.grid(sticky=tk.N)
-        
         self.illness_list.place(x=125,y=200)
-
-        self.illness_button=tk.Button(self,text="Mostra malattie",command=lambda : user_illness_show(self),fg="#000000",bg="#D2D2D2")
+        self.illness_button=tk.Button(self,text="Mostra malattie",command=lambda : self.user_illness_show(),fg="#000000",bg="#D2D2D2")
         self.illness_button.place(x=250,y=400)
-        def user_illness_show(self):
-            print(f"IL RISULTATO DELLE MALATTIE={self.db.show_illness(self.db.id)}")
-            l=self.db.show_illness(self.db.id)
-            for row in l:
-                self.illness_list.insert(END,row)
+
+    def user_illness_show(self):
+        print(f"IL RISULTATO DELLE MALATTIE={self.db.show_illness(self.db.id)}")
+        l=self.db.show_illness(self.db.id)
+        for row in l:
+            self.illness_list.insert(END,row)
+    
+    def _clean(self):
+            self.illness_list.delete(0,tk.END)
+            self.controller.show_frame("User_Page")
+
+        
