df64b9cada9f3ab166b2de811985a711504d87e7
HappyJerry2445 <114814548+HappyJerry2445@users.noreply.github.com>
2022-11-21 17:53:24 +0100

    Update database_access.py

--- view/database_access.py
+++ view/database_access.py
@@ -6,6 +6,7 @@ class Database_access:
 	def __init__(self,id=0):
 		self.id=id
 
+	#function to signup a user in the database 
 	def sign_up(lst=[]):
 		mydb = mysql.connector.connect(
 			host="localhost",
@@ -22,7 +23,8 @@ class Database_access:
 		print(result)
 		mydb.commit()
 	
-	def illness_in_list(self,illness_name,illness_description):
+	#function to get the id of the illness 
+	def illness_in_list(self,illness_name):
 		mydb = mysql.connector.connect(
 			host="localhost",
 			user="root",
@@ -53,7 +55,7 @@ class Database_access:
 		print(f"IL RISULTATO DI IN LIST {id}")
 		return id
 					
-
+	#function to add an illness in the database
 	def add_illness(self,illness_name,illness_description):
 		mydb = mysql.connector.connect(
 			host="localhost",
@@ -69,17 +71,19 @@ class Database_access:
 		print(query)
 		print(result)
 
+	#function that control if the illness is in the list, if the illness is in the database it links the illness with a patient
+	#else it adds the illness in the database and then it links the illness with a patient
 	def illness_control(self,illness_name,id_user,illness_description):
-		id_illness=self.illness_in_list(illness_name,illness_description)
+		id_illness=self.illness_in_list(illness_name)
 		print(f"illness={id_illness}")
 		if id_illness>0:
 			self.illness_assosiaciotion(id_user,id_illness)
 		else:
 			self.add_illness(illness_name,illness_description)
-			id_illness=self.illness_in_list(illness_name,illness_description)
+			id_illness=self.illness_in_list(illness_name)
 			self.illness_assosiaciotion(id_user,id_illness)
 
-
+	#function that link the illness with a patient
 	def illness_assosiaciotion(self,id_user,id_illness):
 		mydb = mysql.connector.connect(
 			host="localhost",
@@ -95,8 +99,8 @@ class Database_access:
 		print(pippo)
 		mydb.commit()
 
+	#function that allows login
 	def user_in_list(cf,psw):
-
 		mydb = mysql.connector.connect(
 			host="localhost",
 			user="root",
@@ -127,8 +131,8 @@ class Database_access:
 		print(id)
 		return id
 
-	#change password
-	def change_password(self,id,password):
+	#function that changes the user password
+	def change_password(self,id,password,confirm_password):
 		mydb = mysql.connector.connect(
 			host="localhost",
 			user="root",
@@ -136,24 +140,60 @@ class Database_access:
 			port=3306,
 			database="ssn"
 		) 
-		mycursor = mydb.cursor()
-		print(f"id={id},password={password}")
-		query=f"""
-			UPDATE 
-				PATIENTS
-			SET 
-				password = "{password}"
-			WHERE
-				id = "{id}"
-			
-		"""
-		mycursor.execute(query)
-		result=mycursor.fetchall()
-		mydb.commit()
+		if confirm_password==password:
+			mycursor = mydb.cursor()
+			print(f"id={id},password={password}")
+			query=f"""
+				UPDATE 
+					PATIENTS
+				SET 
+					password = "{password}"
+				WHERE
+					id = "{id}"
+				
+			"""
+			mycursor.execute(query)
+			result=mycursor.fetchall()
+			mydb.commit()
+		else:
+			return -1000
 		print(query)
 		print(result)
 
+#function che non fa nu cazz
 	def show_illness(self,id_user):
+		mydb = mysql.connector.connect(
+			host="localhost",
+			user="root",
+			password="",
+			port=3306,
+			database="ssn"
+		)
+		l=[]
+		mycursor=mydb.cursor()
+		query=f"""
+			SELECT
+				illness.id, illness.name, illness.description
+			FROM
+				illness JOIN patients_illness ON illness.id=patients_illness.illness_fk
+			WHERE
+				patients_illness.patient_fk = "{id_user}"
+		"""
+		mycursor.execute(query)
+		result=mycursor.fetchall()
+		lst=[]
+		for r in result:
+			for i in range(0,3):
+				lst.append(r[i])
+			obj=illness(lst[0],lst[1],lst[2])
+			print(f"malattia QUERY: {obj}")
+			l.append(obj.__str__())
+			lst.clear()
+		print(f"CONTENUTO DI l = {l}")
+		return l
+
+	#function that shows all illnesses from the database
+	def illness_list(self):
 		mydb = mysql.connector.connect(
 			host="localhost",
 			user="root",
@@ -167,51 +207,22 @@ class Database_access:
 			SELECT
 				* 
 			FROM	
-				patients_illness
-			WHERE
-				patient_fk = "{id_user}"
-		"""
-		mycursor.execute(query)
-		result=mycursor.fetchall()
-		for row in result:
-			l.append(row[1])
-		print(l)
-		pippo=f"""
-			SELECT
-				 
-			FROM	
-				patients_illness
-			WHERE
-				patient_fk = "{id_user}"
-		"""
-
-	def illness_list(self):
-		mydb = mysql.connector.connect(
-			host="localhost",
-			user="root",
-			password="",
-			port=3306,
-			database="ssn"
-		)
-		ill=[]
-		
-		mycursor=mydb.cursor()
-		query=f"""
-			SELECT
-				* 
-			FROM	
 				illness
 		"""
 		mycursor.execute(query)
 		result=mycursor.fetchall()
-		for illness in result:
-			ill.append(illness[1])
-			ill.append(illness[2])
-			
-		print(f"MALATTIE: {ill}")
-		print(query)
-		return ill
+		lst=[]
+		for r in result:
+			for i in range(0,3):
+				lst.append(r[i])
+			obj=illness(lst[0],lst[1],lst[2])
+			print(f"malattia QUERY: {obj}")
+			l.append(obj.__str__())
+			lst.clear()
+		print(f"CONTENUTO DI l = {l}")
+		return l
 
+	#function that permits to delete a user's account
 	def delete_user(self,id,password):
 		mydb = mysql.connector.connect(
 			host="localhost",
